59e22733b8e9a69bceebe6d64d4aeab191e76aaf
Sam Rendall <sdrendall@gmail.com>
2014-05-12 16:28:32 -0400

    added function to preprocess vsis before sending them to a multiTiff, updated populateMultiTiff correspondingly

--- populateMultiTiff.m
+++ populateMultiTiff.m
@@ -10,9 +10,13 @@ end
 
 outputPath = cell(size(channels));
 
+% Calculate Target Dimensions
+[maxX, maxY] = getLargestDimensions(imagePaths);
+
 for i = 1:length(channels)
     % read in the first image
     im = imread(imagePaths{1}, channels(i));
+    im = removeZeros(padToMax(im, maxX, maxY));
     outputPath{i} = formatOutputPath(baseOutput, channels(i));
     imwrite(im, outputPath{i});
 end
@@ -23,13 +27,30 @@ end
 
 parfor chan = 1:length(channels)
     currChannel = (channels(chan));
-
     for i = 2:length(imagePaths)
         currIm = imread(imagePaths{i}, currChannel);
+        currIm = removeZeros(padToMax(currIm, maxX, maxY));
         imwrite(currIm, outputPath{chan}, 'WriteMode', 'append')
     end
 end
 
+
+
+function im = padToMax(im, maxX, maxY)
+    marginSize = [maxY, maxX] - size(im);
+    im = padarray(im, marginSize, 'post');
+
+function im = removeZeros(im)
+        nonZero = im(:) ~= 0;
+        im(~nonZero) = min(im(nonZero));
+        im = mat2gray(im)
+
+function [maxX, maxY] = getLargestDimensions(paths)
+    for i = 1:length(paths)
+        info(i) = imfinfo(paths{i})
+    end
+    maxX = max([info(:).Width]);
+    maxY = max([info(:).Height]);
 
 function outputPath = formatOutputPath(basePath, channel)
     [baseDir, baseName] = fileparts(basePath);
---
+++ preprocessAndCompileFullBrainTiff.m
@@ -0,0 +1,60 @@
+function preprocessAndCompileFullBrainTiff(r)
+
+    thumb = cell(size(r));
+    % load thumbs
+    for i = 1:length(r)
+        thumb{i} = imresize(bfGetPlane(r(i), 1), .0625);
+    end
+
+    % Get Crop coords
+    cropSave = uigetfile();
+
+    figure
+    for i = 1:length(thumb)
+        imshow(thumb{i})
+        [x, y] = ginput(4);
+        [ty, tx] = size(thumb{i});
+        % Normalize x,y to thumbnail size
+        crop(i).x = x ./ tx;
+        crop(i).y = y ./ ty;
+    end
+
+    save(cropSave, 'crop')
+
+    % Create individual, preprocessed images
+    outputDir = uigetdir();
+    if ~exist('outputDir', 'dir')
+        mkdir(outputDir)
+    end
+
+    parfor i = 1:length(r)
+        r(i).setSeries(0)
+        xScaleCoEff = (max(crop(i).x) - min(crop(i).x));
+        yScaleCoEff = (max(crop(i).y) - min(crop(i).y));
+        sizeX = round(r(i).getSizeX * xScaleCoEff);
+        sizeY = round(r(i).getSizeY * yScaleCoEff);
+        im = zeros([sizeY, sizeX, 3]);
+        im(:,:,2) = cropROI(bfGetPlane(r(i), 2), crop(i).x, crop(i).y);
+        im(:,:,3) = cropROI(bfGetPlane(r(i), 1), crop(i).x, crop(i).y);
+        outputPath = fullfile(outputPath, ['preProc_', num2str(i), '.tif']);
+        imwrite(im, outputPath)
+    end
+
+    % Save to MultiTiff
+    f = dir(fullfile(outputDir, '*.tif'));
+    multiOut = fullfile(outputDir, 'fullBrain');
+    populateMultiTiff({f(:).name}, multiOut);
+
+
+    function im = cropROI(im, x, y)
+        % Determine Ultimate Image Size
+        xScaleCoEff = (max(crop(i).x) - min(crop(i).x));
+        yScaleCoEff = (max(crop(i).y) - min(crop(i).y));
+        sizeX = round(r(i).getSizeX * xScaleCoEff);
+        sizeY = round(r(i).getSizeY * yScaleCoEff);
+        % Find start points
+        [nr, nc] = size(im);
+        minX = round(min(x) * nc);
+        minY = round(min(y) * nr);
+        im = im(minY:minY+sizeY-1, minX:minX+sizeX-1);
+    end
